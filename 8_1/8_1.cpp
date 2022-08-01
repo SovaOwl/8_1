5042dfaed99d3c369d0e26e6df9315caee5c4c0b
SovaOwl <sova27755@gmail.com>
2022-08-01 15:14:09 +0300

    First commit

---
+++ 8_1/8_1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cstring>
+
+int function(int forbidden_length, int l, int* flag)
+{
+	if (l == forbidden_length)
+	{
+		return 1;
+	}
+	*flag = l;
+	return 0;
+}
+
+using namespace std;
+
+int main()
+{
+	system("chcp 1251");
+
+	string str = "<3";
+	int forbidden_length = -999, f = 0;
+
+	cout << "Введите запретную длину: ";
+	cin >> forbidden_length;
+	cout << "Введите слово: ";
+	cin >> str;
+
+	int l = str.length();
+
+	int function_res = function(forbidden_length, l, &f);
+	if (function_res == 0)
+	{
+		cout << "Длина слова '" << str << "' равна " << l;
+	}
+	else if (function_res == 1)
+	{
+		cout << "Вы ввели слово запретной длины! До свидания.";
+	}
+}
